e224f7855c71a7aea48340718cae283d1ece6696
WoosterUK <WoosterUK@users.noreply.github.com>
2020-07-05 14:50:39 +0100

    Add math.js

--- js/Controller.js
+++ js/Controller.js
@@ -1,3 +1,7 @@
+var jQueryScript = document.createElement('script');  
+jQueryScript.setAttribute('src','https://unpkg.com/mathjs@7.0.2/dist/math.min.js');
+document.head.appendChild(jQueryScript);
+
 var QuizmasterApp = {
   name: 'Quizmaster'
 }
