c38f697027898305a23dc9efb7a03c776f26a87a
WoosterUK <WoosterUK@users.noreply.github.com>
2020-07-05 16:37:59 +0100

    First attempt at question typing
    We need question types to define what answers look like -- I'm thinking this needs to be done in terms of underlying variables. We can insist later that the request for Booleans is actually served up as a request for a String, defining a Boolean output, that can be parsed by math.js.

--- js/DataModel.js
+++ js/DataModel.js
@@ -1,1 +1,24 @@
+var question_types = {};
 
+class QuestionType {
+  constructor({name, abbreviation, answer_format, answer_translator = undefined}) {
+    this.name = name;
+    this.abbrev = abbreviation;
+    this.answer_format = answer_format;
+    this.answer_translator = answer_translator;
+  }
+  
+  getName() { return this.name; }
+  getAbbrev() { return this.abbrev; }
+}
+
+question_types.push(
+  new QuestionType(
+    {
+      name: 'Multiple Choice',
+      abbreviation: 'MC',
+      answer_format: [String, Boolean],
+      answer_translator: function(a) { if a[1] { return [a[0], 'correct']; } else { return [a[0], 'incorrect']; } }
+    }
+  )
+);
