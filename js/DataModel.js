abf44c244c81ce995ac13db475d48bae30ea96da
WoosterUK <WoosterUK@users.noreply.github.com>
2020-07-06 11:38:36 +0100

    Make a start to rework answers as classes

--- js/DataModel.js
+++ js/DataModel.js
@@ -5,13 +5,11 @@ class QuestionType {
     name,
     abbreviation,
     answer_format,
-    answer_translator = undefined,
     multiple_answers = true
   }) {
     this.name = name;
     this.abbrev = abbreviation;
     this.answer_format = answer_format;
-    this.answer_translator = answer_translator;
     this.multiple_answers = multiple_answers;
   }
   
@@ -19,13 +17,57 @@ class QuestionType {
   getAbbrev() { return this.abbrev; }
 }
 
+class Answer {
+  constructor({
+    name,
+    description,
+    variable_type,
+    translator = function (x) { return x; }
+  }) {
+    this.name = name;
+    this.desc = description;
+    this.TypeClass = variable_type;
+    this.translator = translator;
+  }
+}
+
+class StringAnswer extends Answer {
+  constructor() {
+    super({
+      name: "Answer text",
+      description: "Type the question text here. See [blah] for reference.",
+      variable_type: String
+    });
+  }
+}
+
+class BooleanAnswer extends Answer {
+  constructor() {
+    super({
+      name: "True/false answer",
+      description: "Type the condition for the correct answer. See [blah] for reference.",
+      variable_type: Boolean
+    });
+  }
+}
+
+class CorrectAnswer extends Answer {
+  constructor() {
+    super({
+      name: "True/false answer",
+      description: "Type the condition for the answer to be correct. See [blah] for reference.",
+      variable_type: Boolean,
+      translator: function(x) { if x { return 'correct'; } else { return 'incorrect'; } }
+    });
+  }
+}
+
 question_types.push(
   new QuestionType(
     {
       name: 'Multiple Choice',
       abbreviation: 'MC',
-      answer_format: [String, Boolean],
-      answer_translator: function(a) { if a[1] { return [a[0], 'correct']; } else { return [a[0], 'incorrect']; } }
+      answer_format: [new StringAnswer(), new CorrectAnswer()]
     }
   )
 );
